8f9b51157e6cc7f57b9ba510d41cf8f945548593
sbuDiction <sibusisonkosi0897@gmail.com>
2024-03-08 13:39:49 +0200

    deploy

--- components/alertStatus.tsx
+++ components/alertStatus.tsx
@@ -18,9 +18,9 @@ const AlertStatus: React.FC<Props> = ({
         <div className={`flex text-center items-center bg-${color}-100 px-4 py-2 text-${color}-700 font-bold justify-center`} >
             {icon}
             {time === 'undefined' ?
-                <p className="text-gray-700 text-sm font-bold pl-1">{alertMessage} {time}</p>
-                :
                 <p className="text-gray-700 text-sm font-bold pl-1">{alertMessage}</p>
+                :
+                <p className="text-gray-700 text-sm font-bold pl-1">{alertMessage} {time}</p>
             }
         </div>
     );
