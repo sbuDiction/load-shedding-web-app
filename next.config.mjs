abe03e322013c8f8c3d3099c8ea42d0ddf8adffe
sbuDiction <sibusisonkosi0897@gmail.com>
2024-03-11 12:20:34 +0200

    fix#7: deployment fix :construction_worker_man:

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath:'/load-shedding-web-app',
-  assetPrefix:'/load-shedding-web-app',
-  output: 'export'
 };
 
 export default nextConfig;
